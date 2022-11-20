cccff5ab77a2823c2eb243131d06a0c6d058eab8
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-11-20 02:12:31 +0000

    working on class SudokuSolver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -10,7 +10,7 @@ class SudokuSolver {
 
     //Loop and find the character is digit or .
     for(let i=0;i<lengthStr;i++){
-      if(puzzleString[i] != "." && !(puzzleString[i] >= "0" && puzzleString[i] <= "9")){
+      if(puzzleString[i] != "." && !(puzzleString[i] >= "1" && puzzleString[i] <= "9")){
         return false;
       }
     }
@@ -18,15 +18,27 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+    let rowDict = { "A": [0,8], "B":[9,17], "C":[18,26], "D":[27,35], "E":[36,44], "F":[45,53], "G":[54,62], "H":[63,71], "I":[72,80] };
+    let checkRow = rowDict[row];
+    for(let i=checkRow[0];i<=checkRow[1];i++){
+      if(value == puzzleString[i]){
+        return false;
+      }
+    }
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    for(let i=column-1;i<puzzleString.length;i=i+9){
+      if(value == puzzleString[i]){
+        return false;
+      }
+    }
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    
   }
 
   solve(puzzleString) {
