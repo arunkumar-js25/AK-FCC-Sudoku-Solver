082b1ee31bb81b9c659968621f343ac5779d4279
Arun Kumar <>
2022-11-20 01:49:31 +0000

    controllers function - validate

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,20 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    
+    var lengthStr = puzzleString.length;
+    //Check count is 81
+    if(lengthStr != 81){
+      return false;
+    }
+
+    //Loop and find the character is digit or .
+    for(let i=0;i<lengthStr;i++){
+      if(puzzleString[i] != "." && !(puzzleString[i] >= "0" && puzzleString[i] <= "9")){
+        return false;
+      }
+    }
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
